2ccc19a4de49176d9a9fbab60cb06e3144c3bfd1
arashalaei <arashalaei22@gmail.com>
2021-05-31 17:40:42 +0430

    Chromosome & fitness function

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
+from Chromosome import Chromosome
+
 if __name__ == '__main__':
-    print('AI project 2')
+    _map = ['_', '_', '_', '_', 'G', '_', 'M', 'L', '_', '_', 'G', '_']
+    population = []
+    Number_of_population = 200
+
+    for i in range(Number_of_population):
+        c = Chromosome(_map)
+        population.append(c)
+    
+    sample = population[51]      # To test
+    print(sample.get_string())   # Chromosome genes string
+    print(sample.get_fitness())  # fitness valus
