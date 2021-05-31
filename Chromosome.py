96147e7c2cf71360c600de76fcd670d5c58e0bad
arashalaei <arashalaei22@gmail.com>
2021-05-31 23:12:09 +0430

    Crossover

--- Chromosome.py
+++ Chromosome.py
@@ -1,17 +1,20 @@
 import random
 
 class Chromosome:
-    _map: list
-    __actions = ['0', '0', '0', '1', '2']
+    __map: list
+    __actions = ['0', '0', '0', '0', '1', '2']
     __string: str 
     __fitness: int
 
     def __init__(self, _map) -> None:
-        self._map = _map
+        self.__map = _map
         self.__string = list()
         self.__fitness = 0
         self.__create()
         self.__calc_fitness()
+
+    def get_map(self) -> list:
+        return self.__map
 
     def get_string(self) -> list:
         return self.__string
@@ -19,8 +22,12 @@ class Chromosome:
     def get_fitness(self) -> list:
         return self.__fitness
 
+    def set_string(self, new_string: list) -> None:
+        self.__string = new_string
+        self.__calc_fitness()
+
     def __create(self) -> None:
-        for i in range(len(self._map)):
+        for i in range(len(self.__map)):
             random.shuffle(self.__actions)
             self.__string.append(self.__actions[random.randint(0, len(self.__actions) - 1)])
 
@@ -33,7 +40,7 @@ class Chromosome:
                 if self.__string[i+1] == '1':     # jump in last level
                     score = score + 2
                 break
-            if self._map[location] == 'G':
+            if self.__map[location] == 'G':
                 if i-1 >= 0 and self.__string[i] == '0' and self.__string[i-1] == '1':
                     score = score + 2
                 elif self.__string[i] == '1':
@@ -42,10 +49,10 @@ class Chromosome:
                     max_path = max(max_path, path-1)
                     path = 0
                     __win__ = False
-            elif self._map[location] == 'M':
+            elif self.__map[location] == 'M':
                 if not (self.__string[i] == '1' and self.__string[i+1] == '0'):
                     score = score + 2
-            elif self._map[location] == 'L':
+            elif self.__map[location] == 'L':
                 if not (self.__string[i] == '2' and self.__string[i+1] == '0'):  # lost
                     max_path = max(max_path, path-1)
                     path = 0
--- main.py
+++ main.py
@@ -1,4 +1,25 @@
+import  random 
 from Chromosome import Chromosome
+
+def crossover(first_chromosome: Chromosome, second_chromosome: Chromosome, type: str = "one_point") -> tuple:
+    _map = first_chromosome.get_map()
+    fisrt_children = Chromosome(_map)
+    second_children = Chromosome(_map)
+
+    if type == 'one_point':
+        offspring = random.randint(1, len(first_chromosome.get_string()) - 1)
+        fisrt_children.set_string(first_chromosome.get_string()[:offspring] + second_chromosome.get_string()[offspring:])
+        second_children.set_string(second_chromosome.get_string()[:offspring] + first_chromosome.get_string()[offspring:] )
+        return fisrt_children, second_children
+    elif type == 'two_point':
+        a = random.randint(1, len(first_chromosome.get_string()) - 1)
+        b = random.randint(1, len(first_chromosome.get_string()) - 1)
+        offspring_1 = min(a, b)
+        offspring_2 = max(a, b)
+        fisrt_children.set_string(first_chromosome.get_string()[:offspring_1] + second_chromosome.get_string()[offspring_1:offspring_2] + first_chromosome.get_string()[offspring_2:])
+        second_children.set_string(second_chromosome.get_string()[:offspring_1] + first_chromosome.get_string()[offspring_1:offspring_2] + second_chromosome.get_string()[offspring_2:])
+        return fisrt_children, second_children 
+
 
 if __name__ == '__main__':
     _map = ['_', '_', '_', '_', 'G', '_', 'M', 'L', '_', '_', 'G', '_']
@@ -10,5 +31,19 @@ if __name__ == '__main__':
         population.append(c)
     
     sample = population[51]      # To test
-    print(sample.get_string())   # Chromosome genes string
-    print(sample.get_fitness())  # fitness valus
+    print(sample.get_string(), sample.get_fitness())   # Chromosome genes string & ftness
+
+    print('######')
+    c1 = Chromosome(_map)
+    c2 = Chromosome(_map)
+    
+    print(c1.get_string(), c1.get_fitness())
+    print(c2.get_string(), c2.get_fitness())
+    print('crossover')
+    children = crossover(c1, c2, type='two_point')
+    fisrt_child = children[0]
+    second_child = children[1]
+    print(fisrt_child.get_string(), fisrt_child.get_fitness())
+    print(second_child.get_string(), second_child.get_fitness())
+
+
