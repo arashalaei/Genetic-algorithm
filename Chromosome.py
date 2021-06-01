2824c21e306d6c60760a6386a1115529e9e4ea19
elahe <elahesdi1378@gmail.com>
2021-06-01 19:49:15 +0430

    charts

---
+++ Chromosome.py
@@ -0,0 +1,81 @@
+import random
+
+class Chromosome:
+    __map: list
+    __actions = ['0', '0', '0', '0', '1', '2']
+    __string: str 
+    __fitness: int
+
+    def __init__(self, _map ,type) -> None:
+        self.__map = _map
+        self.__string = list()
+        self.__fitness = 0
+        self.__create()
+        self.__calc_fitness(type)
+
+    def get_map(self) -> list:
+        return self.__map
+
+    def get_string(self) -> list:
+        return self.__string
+
+    def get_fitness(self) -> list:
+        return self.__fitness
+
+    def set_string(self, type, new_string: list) -> None:
+        self.__string = new_string
+        self.__calc_fitness(type)
+
+    def __create(self) -> None:
+        for i in range(len(self.__map)):
+            random.shuffle(self.__actions)
+            self.__string.append(self.__actions[random.randint(0, len(self.__actions) - 1)])
+
+    def mutate(self, type, possibility = 0.1):
+        l = []
+        if possibility == 0.1:
+            l = [0, 0, 0, 0, 0, 0, 0, 0, 0, 1]
+        elif possibility == 0.5:
+            l = [0, 1]
+
+        if l[random.randint(0, len(l) - 1)] == 1:
+            gen_number = random.randint(0, len(self.__map) - 1)
+            self.__string[gen_number] = self.__actions[random.randint(0, len(self.__actions) - 1)]
+            self.__calc_fitness(type)
+
+    def __calc_fitness(self, _win_mode_) -> None:
+        path = location = 1
+        score = max_path = 0
+        __win__ = True
+        for i in range(len(self.__string)):
+            if location == len(self.__string) - 1:
+                if self.__string[i+1] == '1':     # jump in last level
+                    score = score + 2
+                break
+            if self.__map[location] == 'G':
+                if i-1 >= 0 and self.__string[i] == '0' and self.__string[i-1] == '1':
+                    score = score + 2
+                elif self.__string[i] == '1':
+                    score = score + 2
+                elif not (self.__string[i] == '1'):  # lost
+                    max_path = max(max_path, path-1)
+                    path = 0
+                    __win__ = False
+            elif self.__map[location] == 'M':
+                if not (self.__string[i] == '1' and self.__string[i+1] == '0'):
+                    score = score + 2
+            elif self.__map[location] == 'L':
+                if not (self.__string[i] == '2' and self.__string[i+1] == '0'):  # lost
+                    max_path = max(max_path, path-1)
+                    path = 0
+                    __win__ = False
+            if self.__string[i] == '1':
+                score = score - 0.5
+            location = location + 1
+            path = path + 1
+        max_path = max(max_path, path)
+        score = score + max_path
+        if __win__ and _win_mode_:
+            score = score + 5
+
+        self.__fitness = score
--- main.py
+++ main.py
@@ -1,66 +1,130 @@
-import math
+import random
+from Chromosome import Chromosome
+# noinspection PyUnresolvedReferences
+import matplotlib.pyplot as plt
+def sort_population(population:list):
+    population.sort(key=lambda x:x.get_fitness(),reverse=True)
 
-_map = ['_', '_', '_', '_', 'G', '_', 'M', 'L', '_', '_', 'G', '_']
+def averaging(population) -> float:
+    sum = 0
+    for chromosome in population:
+        sum += chromosome.get_fitness()
+    
+    return sum / len(population)
 
+### Crossover ###
+def crossover(first_chromosome: Chromosome, second_chromosome: Chromosome, type: str = "one_point") -> tuple:
+    _map = first_chromosome.get_map()
+    fisrt_children = Chromosome(_map , False)
+    second_children = Chromosome(_map, False)
 
-def fitness_value(chromosome, _win_mode_):
-    path = location = 1
-    score = max_path = 0
-    __win__ = True
-    for i in range(len(chromosome)):
-        if location == len(chromosome) - 1:
-            if chromosome[i+1] == '1':     # jump in last level
-                score = score + 2
+    if type == 'one_point':
+        offspring = random.randint(0, len(first_chromosome.get_string()))
+        fisrt_children.set_string(False, first_chromosome.get_string()[:offspring] + second_chromosome.get_string()[offspring:])
+        second_children.set_string(False ,second_chromosome.get_string()[:offspring] + first_chromosome.get_string()[offspring:] )
+    elif type == 'two_point':
+        a = random.randint(0, len(first_chromosome.get_string()))
+        b = random.randint(0, len(first_chromosome.get_string()))
+        while b == a:
+            b = random.randint(0, len(first_chromosome.get_string()))
+        offspring_1 = min(a, b)
+        offspring_2 = max(a, b)
+        fisrt_children.set_string(False,first_chromosome.get_string()[:offspring_1] + second_chromosome.get_string()[offspring_1:offspring_2] + first_chromosome.get_string()[offspring_2:])
+        second_children.set_string(False,second_chromosome.get_string()[:offspring_1] + first_chromosome.get_string()[offspring_1:offspring_2] + second_chromosome.get_string()[offspring_2:])
+    
+    return fisrt_children, second_children 
+    
+### Selection ###
+def select(population: list ,type = 1):
+    sort_population(population)
+    selection_length = len(population)
+    if selection_length % 2 != 0:
+        selection_length -= 1
+    selection_length /= 2
+    if selection_length % 2 != 0:
+        selection_length += 1
+    if type == 1:
+        return population[:int(selection_length)]
+    elif type == 2:
+        copy_population = population[:] # Copy of population
+        output_list = []
+        for i in range(int(selection_length)):
+            random.shuffle(copy_population)
+            output_list.append(copy_population.pop(0))
+        return output_list
+
+if __name__ == '__main__':
+    _map = ['_', '_', 'G', '_', 'G', '_', 'M', 'L', '_', '_', 'G', '_','G', '_', 'G', '_', 'G', '_', 'M', 'L', '_', '_', 'G', '_']
+    # first method
+    population = []
+    number_of_population = 400
+    mean_list = []
+    # 1) initial population
+    for i in range(number_of_population):
+        c = Chromosome(_map , False)
+        population.append(c)
+    
+    sort_population(population)
+    max_fitness = population[0].get_fitness()
+    counter = 0
+    iterate_number = 10
+    print(max_fitness)
+    best_fitness = []
+    worst_fitness = []
+    while True:
+        if counter == iterate_number:
             break
-        if _map[location] == 'G':
-            if i-1 >= 0 and chromosome[i] == '0' and chromosome[i-1] == '1':
-                score = score + 2
-            elif chromosome[i] == '1':
-                score = score + 2
-            elif not (chromosome[i] == '1'):  # lost
-                max_path = max(max_path, path-1)
-                path = 0
-                __win__ = False
-        elif _map[location] == 'M':
-            if not (chromosome[i] == '1' and chromosome[i+1] == '0'):
-                score = score + 2
-        elif _map[location] == 'L':
-            if not (chromosome[i] == '2' and chromosome[i+1] == '0'):  # lost
-                max_path = max(max_path, path-1)
-                path = 0
-                __win__ = False
-        if chromosome[i] == '1':
-            score = score - 0.5
-        location = location + 1
-        path = path + 1
-    max_path = max(max_path, path)
-    score = score + max_path
-    if __win__ and _win_mode_:
-        score = score + 5
+        # 2)Selection
+        selected_list = select(population , 2)
+        # 3)Crossover
+        epoch = []
+        for i in range(len(selected_list)):
+            a = random.randint(0,  len(selected_list) - 1)
+            b = random.randint(0,  len(selected_list) - 1)
+            while b == a:
+                b = random.randint(0,  len(selected_list) - 1)
 
-    return score
+            children = crossover(selected_list[a], selected_list[b],"two_point")
+            epoch.append(children[0])
+            epoch.append(children[1])
+        # 4)Mutation
+        for chromosome in epoch:
+            chromosome.mutate(False, 0.5)
+        
+        mean_list.append(averaging(epoch))
 
-test = ['0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0']
-test1 = ['0', '0', '1', '1', '0', '0', '0', '0', '0', '0', '0', '0']
-test2 = ['0', '0', '0', '0', '0', '0', '1', '0', '0', '0', '0', '1']
+        population[10:] = epoch[:]
 
-f = [test1, test2, test]
-# print(fitness_value(test))
+        counter += 1
+        sort_population(population)
+        best_fitness.append(population[0].get_fitness())
+        worst_fitness.append(population[-1].get_fitness())
+        if population[0].get_fitness() > max_fitness:
+            max_fitness = population[0].get_fitness()
+            counter = 0
 
+    print(population[0].get_string(), max_fitness)
+    print(mean_list)
+    print(best_fitness)
+    print(worst_fitness)
 
-def choice(chromosomes, mode):
-    __my__dict__ = {}
-    for c in chromosomes:
-        __my__dict__[str(c)] = fitness_value(c, True)
-    mark_list = sorted(__my__dict__.items(), key=lambda x: x[1], reverse=True)
-    sort_dict = dict(mark_list)
-    len_of_best_chromosomes = math.ceil(len(sort_dict) / 2)
-    temp = sort_dict.keys()
-    return temp
-
-
-print(choice(f, 1))
-
-
+    # draw chart
+    generation = []
+    for i in range(len(mean_list)):
+        generation.append(i+1)
+    # mid
+    plt.plot(generation, mean_list, label='mid')
+    plt.xlabel('generation')
+    plt.ylabel('fitness')
+    plt.legend()
+    plt.show()
+    # best mid worst
+    plt.plot(generation, mean_list, label='mid')
+    plt.plot(generation, best_fitness, label='best')
+    plt.plot(generation, worst_fitness, label='worst')
+    plt.xlabel('generation')
+    plt.ylabel('fitness')
+    plt.legend()
+    plt.show()
 
 
